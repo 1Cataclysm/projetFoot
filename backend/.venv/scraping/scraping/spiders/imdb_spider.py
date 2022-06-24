d4a917fe947e45638d8b834572e34ebf8a5b6cb5
slabla <stramote@gmail.com>
2022-06-24 12:11:45 +0200

    fix

--- backend/.venv/scraping/scraping/spiders/imdb_spider.py
+++ backend/.venv/scraping/scraping/spiders/imdb_spider.py
@@ -1,22 +1,33 @@
 import scrapy
+import os
 
 
 class ImdbSpiderSpider(scrapy.Spider):
     name = 'imdb_spider'
-    allowed_domains = ['imdb.com']
-    start_urls = ["https://www.imdb.com/search/title/?groups=top_250"]
+    allowed_domains = ['footmercato.net']
+    #allowed_domains = ['imdb.com']
+    start_urls = ["https://www.footmercato.net/joueur/kylian-mbappe/statistique-but?season=2021&club=psg&competitionType=championship&competition=8879186764835452557&position="]
 
     def parse(self, response):
         print ("ouioui")
-        movies = response.xpath("//div[@class='lister-item-content']")
-        for movie in movies:
-            movie_name = movie.xpath(".//h3//a//text()").get()
-            movie_link = "https://www.imdb.com" + movie.xpath(".//h3//a//@href").get()
-            movie_rating = movie.xpath(".//div[@class='ratings-bar']//strong//text()").get()
+        scores = response.xpath("//div[@class='stats']//tbody//tr")
+        #movies = response.xpath("//div[@class='lister-item-content']")
+        print (scores)
+        print ("ouioui")
+        for score in scores:
+            equipe_name = score.xpath(".//td[@class='stats__title']//a//text()").get()
+            player_time_score = score.xpath(".//td[@class='stats__time']//text()").get()
+            #movie_name = movie.xpath(".//h3//a//text()").get()
+            #movie_link = "https://www.imdb.com" + movie.xpath(".//h3//a//@href").get()
+            #movie_rating = movie.xpath(".//div[@class='ratings-bar']//strong//text()").get()
 
             yield{
-                    'movie name':movie_name,
-                    'movie link':movie_link,
-                    'movie_rating':movie_rating
+                    'match':equipe_name,
+                    'time_score':player_time_score
+                    #'movie link':movie_link,
+                    #'movie_rating':movie_rating
                   }
-        
+
+        #os.system('scrapy crawl imdb_spider -o imdb_mbappe2.csv')
+
+        #https://www.footmercato.net/joueur/kylian-mbappe/statistique-but?season=2021&club=psg&competitionType=championship&competition=8879186764835452557&position=
--- backend/.venv/server.py
+++ backend/.venv/server.py
@@ -1,11 +1,14 @@
 from flask import Flask
+import json
 
+jsonFilePath = open('./.venv/scraping/scraping/spiders/json_file.json')
 app = Flask(__name__)
 
 # Members api route
 @app.route("/members")
 def members():
-    return {"members": ["Member1", "Member2", "Memberss3"]}
+    data = json.load(jsonFilePath)
+    return {"members": ["Member1", "Member2", "Memberss3aazaz"]} #data#{"tag":["oui"]}#{"scraping/scraping/spiders/json_file.json"}#
 
 if __name__ == "__main__":
     app.run(debug=True)
